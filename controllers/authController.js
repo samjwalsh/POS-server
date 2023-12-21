186e772d8279dc5a3bef03c3922490406d75df3a
Sam Walsh <samjwalsh05@gmail.com>
2023-12-21 12:27:10 +0000

    small fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,10 @@ const dotenv = require('dotenv');
 dotenv.config({ path: __dirname + '/.env' });
 
 const auth = (req, res, next) => {
-  if (req.body.key !== process.env.SYNC_KEY)
-    console.log('Unauthorised access blocked');
-  return res.status(403).send('Forbidden');
+  if (req.body.key !== process.env.SYNC_KEY) {
+    console.log('Blocked unauthorised access');
+    return res.status(403).send('Forbidden');
+  }
   next();
 };
 
